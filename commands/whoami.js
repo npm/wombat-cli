8005b1675f29a0ed9986d7f1972b9a3b7cdc88af
C J Silverio <ceejceej@gmail.com>
2016-04-14 13:31:00 -0700

    Eliminate npmconf in favor of rc + registry-auth-token.

--- commands/whoami.js
+++ commands/whoami.js
@@ -1,33 +1,18 @@
 var
-	npmconf  = require('npmconf'),
 	Registry = require('../lib/registry'),
 	report   = require('../lib/report')
 	;
 
 function whoami(argv)
 {
-	npmconf.load(function(err, config)
+	Registry.authed('GET', '/-/whoami', function(err, res, body)
 	{
 		if (err)
 			return report.failure('whoami', err.message);
+		if (!body || !body.username)
+			return report.failure('whoami', 'unexpected registry response! ' + JSON.stringify(body));
 
-		var registry = config.get('registry');
-		if (!registry)
-			return report.failure('whoami', 'no registry set');
-
-		var auth = config.getCredentialsByURI(registry);
-		if (auth.username)
-			return report.success('whoami', auth.username);
-
-		Registry.authed('GET', '-/whoami', function(err, res, body)
-		{
-			if (err)
-				return report.failure('whoami', err.message);
-			if (!body || !body.username)
-				return report.failure('whoami', 'unexpected registry response! ' + JSON.stringify(body));
-
-			console.log(body.username);
-		});
+		console.log(body.username);
 	});
 	argv._handled = true;
 }
