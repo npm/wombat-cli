de0e792d6fcccf42018be09556969bbd502dcec7
C J Silverio <ceejceej@gmail.com>
2016-04-14 10:02:12 -0700

    Some small advancement.

--- commands/hook.js
+++ commands/hook.js
@@ -6,7 +6,7 @@ var
 
 function hooks(argv)
 {
-	// TODO: should just respond with help, I think, or docs
+	// set this here and it's set for all the subcommands
 	argv._handled = true;
 }
 
@@ -17,27 +17,34 @@ hooks.add = function add(argv)
 		' to ' + chalk.yellow(argv.url) +
 		' with shared secret ' + chalk.red(argv.secret)
 	);
-	process.exit();
 };
 
 hooks.rm = function rm(argv)
 {
-	report.success('hook rm', 'un-hooking ' + chalk.yellow(argv.pkg) +
-		' from ' + chalk.yellow(argv.url)
-	);
+	report.success('hook rm', 'un-hooking ' + chalk.yellow(argv.id));
 };
 
 hooks.ls = function ls(argv)
 {
-	// TODO
+	var uri = 'v1/hooks/hook';
+	if (argv.pkg)
+		uri += '/' + encodeURIComponent(argv.pkg);
+
+	Registry.authed('GET', uri, function(err, res, body)
+	{
+		if (err)
+			return report.failure('hook ls', err.message);
+		if (!body || res.statusCode < 200 || res.statusCode >= 400)
+			return report.failure('hook ls', res.statusCode + ' ' + JSON.stringify(body));
+
+		// TODO format the response
+	});
 };
 
 hooks.test = function test(argv)
 {
 	// TODO
-	report.success('hook test', 'testing hook on ' + chalk.yellow(argv.pkg) +
-		' to ' + chalk.yellow(argv.url)
-	);
+	report.success('hook test', 'testing hook id ' + chalk.yellow(argv.id));
 };
 
 function noop() {}
@@ -45,16 +52,20 @@ function noop() {}
 function builder(yargs)
 {
 	return yargs
-		.command('add <pkg> <url> <secret>', 'add a webhook', noop, hooks.add)
-		.command('rm <pkg> <url>', 'remove a webhook', noop, hooks.rm)
-		.command('ls [pkg]', 'list webhooks', noop, hooks.ls)
-		.command('test <pkg> <url>', 'test a webhook', noop, hooks.test)
+		.command('ls [pkg]', 'list your hooks', noop, hooks.ls)
+		.command('add <pkg> <url> <secret>', 'add a hook to the named package', noop, hooks.add)
+		.command('update <id> <url> [secret]', 'update an existing hook', noop, hooks.add)
+		.command('rm <id>', 'remove a hook', noop, hooks.rm)
+		.command('test <id>', 'test a hook', noop, hooks.test)
+		.example('$0 hook add lodash https://example.com/ my-shared-secret')
+		.example('$0 hook ls lodash')
+		.example('$0 hook rm id-ers83f')
 	;
 }
 
 module.exports = {
 	command: 'hook',
-	describe: 'control your webhooks',
+	describe: 'control your hooks',
 	builder: builder,
 	handler: hooks
 };
